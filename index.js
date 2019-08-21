c81361ce1a0eb1b8e5a9cede2f20bc8e1aeb36bf
Katie Hansen <khansen0415@gmail.com>
2019-08-21 14:59:54 -0400

    added functionality for all api calls

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 'use strict';
  
+//GLOBAL VARIABLE
+const wiki = {
+    baseUrl: 'https://en.wikipedia.org/w/api.php'
+}
+
 
 const edamam = {
     baseUrl: 'https://api.edamam.com/search',
@@ -7,16 +12,16 @@ const edamam = {
     appId: '4a4c4ccb'
 }
 
-const nutritionIx = {
-    baseUrl: 'https://trackapi.nutritionix.com/v2/search/instant',
-    apiKey: 'b16e2207c82b607f8a8eea08a8a05139',
-    appId: '24e582af'
+const ndb = {
+    baseUrl: 'https://api.nal.usda.gov/ndb/search',
+    foodSearchUrl: 'https://api.nal.usda.gov/ndb/reports',
+    apiKey: 'Cl2AGWhT8LjHokAVdGRjQBMufFIaLgOqfOUPAXsy'
 }
 
 
 
 
-//format parameters
+//FORMAT PARAMETERS
 function formatParams(params){
     const paramString = Object.keys(params).map(key =>`${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
     return paramString.join('&');
@@ -26,11 +31,68 @@ function formatParams(params){
 
 
 
-//make html for each element
-function makeHtml(responseJson){
+//MAKE HTML ELEMENTS FOR DATA
+
+//make html for title
+function makeTitleHtml(food){
+    $('#js-searched-food').empty();
+
+    const firstLetter = food.charAt(0).toUpperCase();
+    const restOfTitle = food.slice(1);
+    const title = firstLetter + restOfTitle;
+
+    $('#js-searched-food').append(`
+    <h2>${title}</h2>
+        `);
+}
+
+
+//make html for wiki data
+function makeWikiHtml(responseJson){
+    $('#js-results-info').empty();
+
+    const pageId = responseJson.query.pageids[0];
+    const data = responseJson.query.pages[pageId];
+  
+    $('#js-results-info').append(`
+    <img src=${data.thumbnail.source}>
+    <p>${data.extract}</p>
+    <a href="https://en.wikipedia.org/wiki/${data.title}" target="_blank">Read More</a>
+    `);
+}
+
+
+//make html for nutrient data
+function makeNutrientHtml(responseJson){
+    $('#js-results-nutrition').empty();
+
+    const nutrient = responseJson.report.food.nutrients;
+
+    $('#js-results-nutrition').append(`
+        <tr>
+            <th>Nutrient</th>
+            <th>Amount</th>
+        </tr>
+        `);
+
+    for (let i = 0; i < nutrient.length; i++){
+        $('#js-results-nutrition').append(`
+            <tr>
+                <td>${nutrient[i].name}</td>
+                <td><span>${nutrient[i].value}</span> <span>${nutrient[i].unit}</span></td>
+            </tr>
+        `);
+    }
+}
+
+
+
+//make html for recipe data
+function makeRecipeHtml(responseJson){
     $('#js-results-list').empty();
 
     const food = responseJson.hits;
+
     for (let i = 0; i < 10; i++){
         $('#js-results-list').append(`<li class="recipe">
         <img class="image" src="${food[i].recipe.image}">
@@ -46,7 +108,87 @@ function makeHtml(responseJson){
 
 
 
-//fetch api
+
+
+//FETCH DATA FROM APIS
+
+//fetch wiki food origin data
+function fetchWikiData(food){
+    const params = {
+        origin: '*',
+        action: 'query',
+        format: 'json',
+        prop: 'extracts|pageimages',
+        indexpageids: 1,
+        redirects: 1,
+        exchars: 1200,
+        exsectionformat: 'plain',
+        piprop: 'name|thumbnail|original',
+        pithumbsize: 250,
+        titles: food
+    }
+
+    const formatted = formatParams(params);
+    const url = wiki.baseUrl + '?' + formatted;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(responseJson => {
+            makeWikiHtml(responseJson);
+        })
+        .catch(err => console.log(err));
+}
+
+
+//fetch food number (ndbno)
+function fetchNutrientNumber(food){
+    const params = {
+        format: 'json',
+        q: `${food}, raw`,
+        api_key: ndb.apiKey
+    }
+
+    const formatted = formatParams(params);
+    const url = ndb.baseUrl + '?' + formatted;
+
+    console.log(url);
+
+    fetch(url)
+        .then(response => response.json())
+        .then(responseJson => {
+            console.log(responseJson);
+            const ndbno = responseJson.list.item[0].ndbno;
+            console.log(ndbno);
+            fetchNutrientData(ndbno);
+        })
+        .catch(err => console.log(err));
+}
+
+//fetch nutrient data
+function fetchNutrientData(ndbno){
+
+    console.log(ndbno);
+    const params = {
+        ndbno: ndbno,
+        format: 'json',
+        type: 'b',
+        api_key: ndb.apiKey
+    }
+
+    const formatted = formatParams(params);
+    const url = ndb.foodSearchUrl + '?' + formatted;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(responseJson => {
+            makeNutrientHtml(responseJson);
+        })
+        .catch(err => console.log(err));
+}
+
+
+
+//fetch recipe data
 function fetchData(food, restriction){
     const params = {
         q: food,
@@ -56,16 +198,12 @@ function fetchData(food, restriction){
     }
 
     const formatted = formatParams(params);
-
     const url = edamam.baseUrl + '?' + formatted;
-
-    console.log(url);
 
     fetch(url)
         .then(response => response.json())
         .then(responseJson => {
-            console.log(responseJson);
-            makeHtml(responseJson);
+            makeRecipeHtml(responseJson);
         })
         .catch(err => console.log(err));
 }
@@ -75,13 +213,18 @@ function fetchData(food, restriction){
 
 
 
+//LISTEN ON FORM AND EXECUTE CODE
+
 //listen on form
 function onSubmit(){
     $('form').on('submit', function(e){
         e.preventDefault();
         const food = $('#js-food').val();
         const restriction = $('#js-health').val();
+        makeTitleHtml(food);
         fetchData(food, restriction);
+        fetchWikiData(food);
+        fetchNutrientNumber(food);
     });
 }
 
